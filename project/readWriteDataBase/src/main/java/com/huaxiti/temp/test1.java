98f18ad18fbdb8e48a241e64e882a0a89f514fb0
huaxi <huaxiti@163.com>
2023-06-27 17:39:40 +0800

    Revert "commit test13"
    This reverts commit c811d80c1176e8fb83fc2b1351695c23c29c3948.

--- project/readWriteDataBase/src/main/java/com/huaxiti/temp/test1.java
+++ project/readWriteDataBase/src/main/java/com/huaxiti/temp/test1.java
@@ -3,6 +3,5 @@ package com.huaxiti.temp;
 public class test1 {
     public static void main(String[] args) {
         System.out.println("test1");
-        System.out.println();
     }
 }
--- project/readWriteDataBase/src/main/java/com/huaxiti/temp/test3.java
+++ project/readWriteDataBase/src/main/java/com/huaxiti/temp/test3.java
@@ -4,6 +4,5 @@ public class test3 {
 
     public static void main(String[] args) {
         System.out.println("test1");
-        System.out.println();
     }
 }
